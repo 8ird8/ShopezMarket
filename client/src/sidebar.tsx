95df8b9c0f93a048a54e68f1e4dccd3a5f12e1e4
Achraf <117481103+8ird8@users.noreply.github.com>
2024-03-07 01:56:57 +0100

    Update sidebar.tsx

--- client/src/sidebar.tsx
+++ client/src/sidebar.tsx
@@ -31,9 +31,10 @@ const Sidebar = () => {
     <>
 {/*     <div className="absolute top-4 left-4"
       onClick={() =>setOpen(true)}>
-      <img src="bars.png" alt="menu"  className=""/> */}
+      <img src="bars.png" alt="menu"  className=""/> 
 
       </div>
+      */}
       <div className="flex flex-row min-h-screen bg-gray-100 text-gray-800">
         <aside className={ `sidebar  md:shadow  ${open ? "transform lg:w-72 transition-transform  bg-slate-600 duration-150 ease-in  hidden lg:flex md:flex" : "flex"}     `}>
           <div className="sidebar-content w-full  px-4 py-6">
